e808973c47fb6e087a33535bcce0226ef9eda0bc
okeyifee <okezieokechukwu94@gmail.com>
2020-11-15 08:42:46 +0100

    built start button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  const [timeLeft, setTimeLeft] = useState( 25 * 60);
 
-  const minutes = Math.floor(timeLeft / 60).toString.padStart(2,'0');
-  const seconds = (timeLeft - minutes * 60).toString().padStart(2, '0');
+function padTime(time){
+  return time.toString().padStart(2, '0');
+}
+
+function App() {
+  const [title, setTitle] = useState('let the countdown begin !!!');
+  const [timeLeft, setTimeLeft] = useState(15);
+
+  function startTimer() {
+    setInterval(() => {
+      setTimeLeft(timeLeft => {
+        if (timeLeft >= 1) return timeLeft -1;
+
+        // reset timer
+        return 0;
+        });
+
+    }, 1000)
+  }
+
+  const minutes = padTime(Math.floor(timeLeft / 60));
+  const seconds = padTime(timeLeft - minutes * 60);
 
 
   return (
     <div className="App">
-      <h2> COUNTDOWN TIMER APP </h2>
+      <h2> {title} </h2>
 
       <div className = "timer">
       <span>{minutes}</span>
@@ -19,7 +37,7 @@ function App() {
       </div>
 
       <div className = "buttons">
-        <button> start </button>
+        <button onClick = {startTimer}> start </button>
         <button> stop </button>
         <button> reset </button>
       </div>
